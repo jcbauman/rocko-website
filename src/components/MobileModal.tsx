fc49f15fbc880197ee5371abdfc622a7c0595bf1
Rocko Bauman <jackrockocasey@gmail.com>
2025-05-25 12:58:55 -0400

    complete ts conversion and upgrade to react 18

--- src/components/MobileModal.tsx
+++ src/components/MobileModal.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import '../css/mobileModal.css';
-import { graduation, super8Logo } from "../images";
+import React from "react";
+import "../css/mobileModal.css";
+import { super8Logo } from "../images";
 
 export const MobileModal = () => {
-
-    return(
-        <div className='mobileModal'>
-            <img className='circularPhoto' src={super8Logo} alt={'home image'}/>
-            <h1>Hello</h1>
-            <h3>For the best experience on this site, please check it out on your computer</h3>
-        </div>
-    )
-}
+  return (
+    <div className="mobileModal">
+      <img className="circularPhoto" src={super8Logo} alt={"home"} />
+      <h1>Hello</h1>
+      <h3>
+        For the best experience on this site, please check it out on your
+        computer
+      </h3>
+    </div>
+  );
+};
